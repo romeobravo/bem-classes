b4f4b447b12486069631b1c3c4b2488baf7582d9
Ruben Buitelaar <rubenbuitelaar@gmail.com>
2016-09-05 22:27:17 +0200

    working version

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # bem-classes
-Easy BEM classes
+Easy (BEM) classes for in your javascript application.
+
+# Install
+```bash
+npm install bem-classes
+```
+
+# Usage
+
+## Require
+You can use any variable name you like.
+```js
+var classes = require('bem-classes')
+import classes from 'bem-classes'
+```
+You can also grab the index.js
+
+## Standard class strings
+Pass the arguments to the imported function and call `.s` to return the class string. You can pass strings or key-value pairs as arguments. The key of a key-value pair only gets added as a class if the value is truthy.
+```js
+classes('block').s
+  // => 'block'
+classes('block', 'column').s
+  // => 'block column'
+classes('block', { column: true }).s
+  // => 'block column'
+classes('block', { column: false }).s
+  // => 'block'      
+```
+
+## BEM class strings
+The first valid class becomes the block class.
+Use `_` as a prefix for an element and `-` or `$` for a modifier.
+```js
+classes('block').s
+  // => 'block'
+classes('block', '-modifier').s
+  // => 'block block--modifier'
+classes('block', '$modifier').s
+  // => 'block block--modifier'
+classes('block', { '-modifier': true }).s
+  // => 'block block--modifier'
+classes('block', { $modifier: true }).s
+  // => 'block block--modifier'
+classes('block__element', '$modifier').s
+  // => 'block__element block__element--modifier'  
+```
+You can extend the returned object with the elem method. This can be useful for building a React class as a block with the elements inside.
+
+```js
+const blockClasses = classes('block', { $modifier: true })
+blockClasses.s
+// => 'block block--modifier'
+blockClasses.elem('element', { $modifier: true). }s
+// => 'block__element block__element--modifier'
+```
+
+# Test
+```
+npm test
+```
+
+# Build
+```
+npm run build
+```
+
+# Develop (watch and rebuild)
+```
+npm run watch
+```
---
+++ bem-classes.js
@@ -0,0 +1,134 @@
+'use strict';
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function isBemModifier(name) {
+  return name.indexOf('$') == 0 || name.indexOf('-') == 0;
+}
+
+function isBemElement(name) {
+  return name.indexOf('_') == 0;
+}
+
+function withoutPrefix(name) {
+  return name.substring(1);
+}
+
+function toBemElement(base, string) {
+  return base + '__' + string;
+}
+
+function toBemModifier(base, string) {
+  return base + '--' + string;
+}
+
+function classFromString(base, string) {
+  if (isBemElement(string)) {
+    return toBemElement(base, withoutPrefix(string));
+  } else if (isBemModifier(string)) {
+    return toBemModifier(base, withoutPrefix(string));
+  } else {
+    return string;
+  }
+}
+
+function classFromObject(base, object) {
+  var classes = [];
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = Object.keys(object)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var key = _step.value;
+
+      if (object[key]) {
+        classes.push(classFromString(base, key));
+      }
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  return classes;
+}
+
+var BemClasses = function () {
+  function BemClasses() {
+    _classCallCheck(this, BemClasses);
+
+    this.classes = [];
+    // this.base = args[0]
+    // this.classes.push(this.base)
+
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    for (var i = 0; i < args.length; i++) {
+      if (_typeof(args[i]) == 'object') {
+        var _classes;
+
+        (_classes = this.classes).push.apply(_classes, _toConsumableArray(classFromObject(this.base, args[i])));
+      } else if (_typeof(args[i] == 'string')) {
+        this.classes.push(classFromString(this.base, args[i]));
+      }
+    }
+  }
+
+  _createClass(BemClasses, [{
+    key: Symbol.iterator,
+    value: function value() {
+      return this.classes;
+    }
+  }, {
+    key: 'elem',
+    value: function elem() {
+      for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+        args[_key2] = arguments[_key2];
+      }
+
+      var element = toBemElement(this.base, args.shift());
+      return new (Function.prototype.bind.apply(BemClasses, [null].concat([element], args)))();
+    }
+  }, {
+    key: 'base',
+    get: function get() {
+      return this.classes[0] || '';
+    }
+  }, {
+    key: 's',
+    get: function get() {
+      return this.classes.join(' ');
+    }
+  }]);
+
+  return BemClasses;
+}();
+
+function createInstance() {
+  for (var _len3 = arguments.length, args = Array(_len3), _key3 = 0; _key3 < _len3; _key3++) {
+    args[_key3] = arguments[_key3];
+  }
+
+  return new (Function.prototype.bind.apply(BemClasses, [null].concat(args)))();
+}
+
+module.exports = createInstance;
